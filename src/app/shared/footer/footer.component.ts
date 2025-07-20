45ac681764f9ace4d563577319d5d32a045c3a7c
prathmesh <prathmeshumredkar@gmail.com>
2025-07-20 13:37:53 +0530

    add changes

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,25 +1,14 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-footer',
-  imports: [ReactiveFormsModule],
+  imports: [],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-  emailForm = new FormGroup({
-    email: new FormControl('')
-  });
-
-  onSubmit() {
-    const emailValue = this.emailForm.value.email;
-    console.log('Submitted Email:', emailValue);
-  }
-
-  resetForm() {
-    this.emailForm.reset();
-  }
+  
 }
 
 
